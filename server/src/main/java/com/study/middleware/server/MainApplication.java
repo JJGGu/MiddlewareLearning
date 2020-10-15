2aa736e97f2de61260f52472e7e1a68fc4f0a36d
JJGGu <haoranxu1996@163.com>
2020-10-15 22:34:34 +0800

    middleware

--- server/src/main/java/com/study/middleware/server/MainApplication.java
+++ server/src/main/java/com/study/middleware/server/MainApplication.java
@@ -1,3 +1,6 @@
+package com.study.middleware.server;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,14 +12,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author: JJGGu
  * @create: 2020-10-15 20:52
  **/
+@Slf4j
 @SpringBootApplication
-public class MyApplication extends SpringBootServletInitializer {
+public class MainApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return super.configure(builder);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
